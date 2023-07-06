bf10d0e703c8ad9185a18c85709847f0d66ad33f
yifanli <yifanlicode@gmail.com>
2023-07-05 23:16:45 -0500

    add and delete

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,31 +1,64 @@
 class WatchlistsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:create, :destroy]
+
+  # show all watchlists
   def index
-    @watchlists = Watchlist.all
+    @watchlists = current_user.watchlists
+    @new_watchlist = Watchlist.new
   end
 
+  # show a single watchlist and its coins
   def show
-    @watchlist = Watchlist.find_by(cryptocurrency_id: @cryptocurrency.id)
   end
 
-  def new
-    @watchlist = Watchlist.new
-  end
-
+  # create a new watchlist
   def create
-    @watchlist = Watchlist.new(watchlist_params)
+    @watchlist = current_user.watchlists.build(watchlist_params)
     if @watchlist.save
-      redirect_to @watchlist
+      redirect_to watchlists_path, notice: "Watchlist created successfully."
     else
-      render 'new'
+      # Handle validation errors
+      render :index
+    end
+  end
+
+  # Update the name of a watchlist
+  def update
+    @watchlist = current_user.watchlists.find(params[:id])
+
+    if @watchlist.update(watchlist_params)
+      redirect_to @watchlist, notice: "Watchlist updated successfully."
+    else
+      render :edit
     end
   end
 
   def destroy
-    @watchlist = Watchlist.find(params[:id])
+    @watchlist = current_user.watchlists.find(params[:id])
     @watchlist.destroy
-    redirect_to watchlists_path, notice: "Coin removed from watchlist successfully."
-    end
+    redirect_to watchlists_path, notice: "Watchlist deleted successfully."
+  end
 
-  private
+  # Add a coin to a watchlist
+  def add_coin
+    watchlist = Watchlist.find(params[:watchlist_id])
+    cryptocurrency = Cryptocurrency.find(params[:cryptocurrency_id])
+    watchlist.cryptocurrencies << cryptocurrency unless watchlist.cryptocurrencies.exists?(cryptocurrency.id)
+    redirect_to cryptocurrencies_path, notice: "Coin added to watchlist successfully."
+  end
+  
+  def remove_coin
+    watchlist = Watchlist.find(params[:watchlist_id])
+    cryptocurrency = Cryptocurrency.find(params[:cryptocurrency_id])
+    watchlist.cryptocurrencies.delete(cryptocurrency) if watchlist.cryptocurrencies.exists?(cryptocurrency.id)
+    redirect_to cryptocurrencies_path, notice: "Coin removed from watchlist successfully."
+  end
+ 
+  
+  def watchlist_params
+    params.require(:watchlist).permit(:name)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,17 @@ Rails.application.routes.draw do
   resources :cryptocurrencies
   resources :watchlists
 
+  resources :watchlists do
+    member do
+      post 'add_coin', to: 'watchlists#add_coin'
+      delete 'remove_coin', to: 'watchlists#remove_coin'
+    end
+  end
+
+  delete 'watchlists/:watchlist_id/remove_coin/:cryptocurrency_id', to: 'watchlists#remove_coin', as: 'watchlist_remove_coin'
+  post 'watchlists/:watchlist_id/add_coin/:cryptocurrency_id', to: 'watchlists#add_coin', as: 'watchlist_add_coin'
+
+
   get 'home/about', to: 'home#about'
   root 'home#index'
 end
